fab98a9a3ac115e3007b5624f0881666218fdd41
NguyenTruong <it10@katsuravn.com>
2024-09-09 10:51:33 +0700

    finish navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
+import { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <img src={assets.nguyen} alt="nguyen-logo" />
-        <ul className='navbar-menu'>
-            <li>home</li>
-            <li>menu</li>
-            <li>mobile-app</li>
-            <li>contact us</li>
-        </ul>
 
-        <div className='navbar-right'>
-             <img src={assets.search} alt="Search icon" />
-            <div className='navbar-search-icon'>
-                <img src={assets.basket} alt="Basket icon" />
-                <div className='dot'></div>
+    const [menu, setMenu] = useState("home")
+
+
+
+    return (
+        <div className="navbar">
+            <img src={assets.nguyen} className='logo' alt="nguyen-logo" />
+            <ul className='navbar-menu'>
+                <li onClick={() => setMenu("home")} className = {menu === "home" ? "active":""}>home</li>
+                <li onClick={() => setMenu("menu")} className = {menu === "menu" ? "active":""}>menu</li>
+                <li onClick={() => setMenu("mobile-app")} className = {menu === "mobile-app" ? "active":""}>mobile-app</li>
+                <li onClick={() => setMenu("contact-us")} className = {menu === "contact-us" ? "active":""}>contact us</li>
+            </ul>
+
+            <div className='navbar-right'>
+                <img src={assets.search} className="search-icon" alt="Search icon" />
+                <div className='navbar-search-icon'>
+                    <img src={assets.basket} className="basket-icon" alt="Basket icon" />
+                    <div className='dot'></div>
+                </div>
+                <button>sign in</button>
+
             </div>
         </div>
-        <button>sign in</button>
-    </div>
-  )
+    )
 }
 
 export default Navbar
